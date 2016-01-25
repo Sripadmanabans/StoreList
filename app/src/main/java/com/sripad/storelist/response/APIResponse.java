2bc83f543f67c67e164849fceedb9a435446c962
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2016-01-25 12:08:43 -0600

    Made model implement parcelable

--- app/src/main/java/com/sripad/storelist/response/APIResponse.java
+++ app/src/main/java/com/sripad/storelist/response/APIResponse.java
@@ -1,5 +1,8 @@
 package com.sripad.storelist.response;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 import com.sripad.storelist.response.stores.Store;
 
@@ -9,12 +12,38 @@ import java.util.List;
  * This is the response that is got when making the api call.
  * Created by Sripad on 1/23/2016.
  */
-public class APIResponse {
+public class APIResponse implements Parcelable {
 
     @SerializedName("stores")
     private List<Store> stores;
 
+    protected APIResponse(Parcel in) {
+        stores = in.createTypedArrayList(Store.CREATOR);
+    }
+
+    public static final Creator<APIResponse> CREATOR = new Creator<APIResponse>() {
+        @Override
+        public APIResponse createFromParcel(Parcel in) {
+            return new APIResponse(in);
+        }
+
+        @Override
+        public APIResponse[] newArray(int size) {
+            return new APIResponse[size];
+        }
+    };
+
     public List<Store> getStores() {
         return stores;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeTypedList(stores);
     }
 }
